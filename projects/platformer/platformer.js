9361271eb476a97beede02f07415131882c6a19f
Braylaae <brayla.ellis@carvercollegiate.org>
2024-09-17 18:48:17 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -36,11 +36,10 @@ $(function () {
 
     // TODO 1
     // Create platforms(500, 300, 200, 20); //
-    Short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-    createPlatform(700,500,200,20)
-    createPlatform()
+    createPlatform(150,700,200,20);
+    createPlatform(340,610.200,20);
 
     
     // TODO 2
@@ -48,8 +47,10 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+createColletable('datebase',420,400,0.5,0.5)
+createColletable('steve',100,400,0.5,0.5)
+createColletable('diamond',400,410,0.5,0.5)
+createColletable('grace' , 200,150,0.5,0.5)
 
     
     // TODO 3
@@ -58,6 +59,9 @@ $(function () {
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
+    createCannon('top',600,1000)
+    createCannon('left',100,200)
+    createCannon('right',200,100)
 
 
     
